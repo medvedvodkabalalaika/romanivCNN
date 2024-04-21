4645c4e4a724c2acbb9e1430b7e4bd6797d42a32
romanivske <82606529+medvedvodkabalalaika@users.noreply.github.com>
2024-04-21 15:18:29 +0300

    Add files via upload

---
+++ statistics/statistics-WINDOWSexample.cmd
@@ -0,0 +1,14 @@
+@echo off
+color 9
+echo Date and time: 2024-04-21-14-55-08
+echo Device: NVIDIA GeForce RTX 3070
+
+echo Number of epochs: 32
+echo Learning rate: 0.00118
+echo Batch size: 64
+
+echo Network accuracy: 7692 out of 10000 (76.92%)
+echo Training time: 584.84 seconds
+
+echo romanivske/romanivCNN
+pause
